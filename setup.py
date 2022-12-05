2add69a3c32c8e74f69507bc6ae1a7f1307d3c3c
freh-g <francesco.gualdi01@estudiant.upf.edu>
2022-12-05 10:54:55 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={'biomapy': ['datasets/*.gz']},
-    version='0.2.2',
+    version='0.2.4',
     install_requires=['wget'],
     long_description='This is a library that has just one function that maps a list of gene IDs from one to another. Supported IDs are Uniprot, entrez, ensembl, and symbol. The usage is straightforward the gene_mapping_many() takes as argument the list of IDs, source IDs and target IDs. The function update_datasets() will update the map datasets directly from uniprot and ncbidb. Future implementations will focus on expanding the possibility of mapping between IDs.',
     author='Francesco Gualdi',
-    license='MIT'
+    license='GPL'
 )
